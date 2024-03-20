68bf7e6a5bcbee3000f573eb2945f692c9ecf25a
Varun Mathiyalagan <140169607+Varun2773@users.noreply.github.com>
2024-03-20 21:26:42 +0530

    jsx crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div className='text-9xl'>App</div>
-  )
-}
+  const name = "varun";
+  const x = 10;
+  const y = 20;
+  const names = ["Brad", "Mary", "Joe", "Sara"];
+  const loggedIn = true;
+  const styles = {
+    color:'red',
+    fontSize: '55px'
+  };
 
-export default App
+
+
+  return (
+    <>
+      <div className="text-7xl">App</div>
+      <p style={styles}>hello {name}</p>
+      <p className="text-7xl">
+        the sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn && <h1>hello member</h1> }
+    </>
+  );
+};
+
+export default App;
