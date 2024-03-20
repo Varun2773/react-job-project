c04beaca7828204401ea9a5b2adb0302615a4e92
Varun Mathiyalagan <140169607+Varun2773@users.noreply.github.com>
2024-03-20 22:46:33 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards"
+import JobListing from "./components/JobListing";
 
 const App = () => {
-  const name = "varun";
-  const x = 10;
-  const y = 20;
-  const names = ["Brad", "Mary", "Joe", "Sara"];
-  const loggedIn = true;
-  const styles = {
-    color:'red',
-    fontSize: '55px'
-  };
-
-
-
   return (
     <>
-      <div className="text-7xl">App</div>
-      <p style={styles}>hello {name}</p>
-      <p className="text-7xl">
-        the sum of {x} and {y} is {x + y}
-      </p>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {loggedIn && <h1>hello member</h1> }
+      <Navbar />
+      <Hero/>
+      <HomeCards />
+      <JobListing />
+
+        {/* 59. minites */}
+      
+    
+      
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
     </>
   );
 };
