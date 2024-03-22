730c8fd734ad2b03319072476d744a8d7ed0a453
Varun Mathiyalagan <140169607+Varun2773@users.noreply.github.com>
2024-03-22 21:50:02 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards"
 import JobListing from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -10,20 +11,14 @@ const App = () => {
       <Hero/>
       <HomeCards />
       <JobListing />
+      <ViewAllJobs />
 
-        {/* 59. minites */}
+    
       
     
       
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      
     </>
   );
 };
